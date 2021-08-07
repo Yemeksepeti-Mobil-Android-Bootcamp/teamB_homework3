1ce87c565f12411ba99131494bbe67fdba0c4e92
MelihcanSrky <sarikayamelihcan-01@outlook.com>
2021-08-07 18:06:46 +0300

    RestaurantAdapter update

--- app/src/main/java/com/example/teambhomework3/utils/adapters/RestaurantAdapter.kt
+++ app/src/main/java/com/example/teambhomework3/utils/adapters/RestaurantAdapter.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
@@ -24,13 +26,13 @@ class RestaurantAdapter(private val restaurantList: ArrayList<Restaurant>, priva
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_restaurant, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.cardviewlayout, parent, false)
         return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val restaurant: Restaurant =restaurantList[position]
-
+        var navController: NavController? = null
         holder.restaurantName.text = restaurant.restaurantName
         holder.circularProgressDrawable.strokeWidth = 5f
         holder.circularProgressDrawable.centerRadius = 30f
@@ -43,6 +45,11 @@ class RestaurantAdapter(private val restaurantList: ArrayList<Restaurant>, priva
             .transition(DrawableTransitionOptions.withCrossFade())
             .placeholder(holder.circularProgressDrawable)
             .into(holder.restaurantImageView)
+
+        holder.itemView.setOnClickListener {
+            navController = Navigation.findNavController(holder.itemView)
+            navController!!.navigate(R.id.action_restaurantsFragment_to_foodsFragment)
+        }
     }
 
     override fun getItemCount(): Int {
