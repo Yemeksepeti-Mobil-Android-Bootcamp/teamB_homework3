7ec5dcdbb40b17784921a0400204a8d8242164d6
MelihcanSrky <sarikayamelihcan-01@outlook.com>
2021-08-10 20:31:39 +0300

    Yemek listesinin veri tabanı bağlantısı

--- app/src/main/java/com/example/teambhomework3/fragments/food/FoodsFragment.kt
+++ app/src/main/java/com/example/teambhomework3/fragments/food/FoodsFragment.kt
@@ -1,19 +1,31 @@
 package com.example.teambhomework3.fragments.food
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.teambhomework3.R
 import com.example.teambhomework3.databinding.FragmentFoodsBinding
+import com.example.teambhomework3.entity.Food
+import com.example.teambhomework3.utils.adapters.FoodAdapter
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.processNextEventInCurrentThread
 
 
 class FoodsFragment : Fragment() {
 
     private var _binding: FragmentFoodsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var myAdapter: FoodAdapter
+    private lateinit var db: FirebaseFirestore
+    private lateinit var foodArrayList: ArrayList<Food>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +37,46 @@ class FoodsFragment : Fragment() {
             findNavController().navigate(R.id.action_foodsFragment_to_addFoodFragment)
         }
 
+        initViews()
         return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    private fun initViews(){
+        binding.recyclerView2.layoutManager = GridLayoutManager(requireContext(),1)
+        binding.recyclerView2.setHasFixedSize(true)
+
+        foodArrayList = arrayListOf()
+        myAdapter = FoodAdapter(foodArrayList,requireContext())
+        binding.recyclerView2.adapter = myAdapter
+
+        db = Firebase.firestore
+
+        getFoods(db)
+    }
+
+
+    private fun getFoods(db: FirebaseFirestore){
+        db.collection("restaurantFoods").get()
+            .addOnSuccessListener { food ->
+                for (doc in food){
+                    foodArrayList.add(
+                        Food(
+                            "${doc.data["foodName"]}",
+                            "${doc.data["foodImage"]}",
+                            "${doc.data["foodPrice"]}",
+                            "${doc.data["foodDescription"]}"
+                        )
+                    )
+                }
+                myAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception->
+                Log.w("errorDocuments","Error getting documents", exception)
+            }
+    }
+
 }
--- app/src/main/java/com/example/teambhomework3/utils/adapters/FoodAdapter.kt
+++ app/src/main/java/com/example/teambhomework3/utils/adapters/FoodAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.teambhomework3.utils.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,14 +8,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.teambhomework3.R
 import com.example.teambhomework3.entity.Food
 
-class FoodAdapter(private val foodNamesList: List<Food>): RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
+class FoodAdapter(private val foodNamesList: List<Food>,private val mContext:Context): RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val foodName: TextView = itemView.findViewById(R.id.foodName)
         val foodImageView : ImageView = itemView.findViewById(R.id.foodImage)
         val foodPrice : TextView = itemView.findViewById(R.id.foodPrice)
+        val circularProgressDrawable = CircularProgressDrawable(mContext)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,7 +28,21 @@ class FoodAdapter(private val foodNamesList: List<Food>): RecyclerView.Adapter<F
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.foodName.text = foodNamesList[position].toString()
+        val food: Food = foodNamesList[position]
+        holder.foodName.text = food.foodName
+        holder.circularProgressDrawable.strokeWidth = 5f
+        holder.circularProgressDrawable.centerRadius = 30f
+        holder.circularProgressDrawable.start()
+
+        Glide
+            .with(mContext)
+            .load(food.foodImage)
+            .centerCrop()
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .placeholder(holder.circularProgressDrawable)
+            .into(holder.foodImageView)
+
+
     }
 
     override fun getItemCount(): Int {
