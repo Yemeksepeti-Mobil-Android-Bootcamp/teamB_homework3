7dd0800144a037423ece4a03a25e293f53489f7b
fatih eren <fatihhernn@gmail.com>
2021-08-12 00:02:45 +0300

    share menu for food detail

--- app/src/main/java/com/example/teambhomework3/fragments/food/FoodDetailFragment.kt
+++ app/src/main/java/com/example/teambhomework3/fragments/food/FoodDetailFragment.kt
@@ -1,14 +1,15 @@
 package com.example.teambhomework3.fragments.food
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.teambhomework3.R
 import com.example.teambhomework3.databinding.FragmentFoodDetail2Binding
+
 
 class FoodDetailFragment : Fragment() {
     private var _binding: FragmentFoodDetail2Binding? = null
@@ -22,11 +23,31 @@ class FoodDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFoodDetail2Binding.inflate(inflater, container, false)
+
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.foodDetailToolbar)
+        (activity as AppCompatActivity?)!!.supportActionBar?.title=""
+
         val view = binding.root
 
         initViews()
         binding.foodDetailToolbarBaslik.text=args.foodDetail.foodName
         return view
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_share_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId== R.id.food_share_menu){
+            val shareIntent= Intent().apply {
+                this.action= Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT,"Yemek Adı : ${args.foodDetail.foodName}")
+                this.type="text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun initViews() {
